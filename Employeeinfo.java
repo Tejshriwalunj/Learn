89c7c1630bf9395a06fab5d47259c4d0f24fb892
Tejshriwalunj <141727348+Tejshriwalunj@users.noreply.github.com>
2023-08-11 16:46:18 +0530

    Add files via upload

---
+++ Employeeinfo.java
@@ -0,0 +1,32 @@
+package Example;
+
+public class Employeeinfo {
+	private int eno;
+	private String ename;
+	private double esal;
+	public Employeeinfo() {
+	}
+	
+	public int getEno() {
+		return eno;
+	}
+	public void setEno(int eno) {
+		this.eno = eno;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	public double getEsal() {
+		return esal;
+	}
+	public void setEsal(double esal) {
+		this.esal = esal;
+	}
+	
+
+}
+
+
